a3e6b34566f660bb2adf1c9948786d3825c78916
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-28 15:09:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import { Navbar, Home, Quiz, Category } from "./components";
+import { Navbar, Home, Category } from "./components";
 
 
 
@@ -12,7 +12,6 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/category" element={<Category />} />
         {/* <Route path="/quiz" element={<Quiz />} /> */}
-        {/* <Route path="/products" element={<Products />} /> */}
       </Routes>
     </div>
   );
