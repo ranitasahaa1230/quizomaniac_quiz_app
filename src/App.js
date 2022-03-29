c57153cac3454ba52f446ed874d96368b87bb026
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-29 13:50:10 +0530

    update rulespage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Navbar, Home, Category, RulesPage, Quiz } from "./components";
 import axios from "axios";
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function App() {
-  const [names,setNames]=useState('');
-  const [questions,setQuestions]=useState('');
-  const [scores,setScores]=useState(0);
+  const [names, setNames] = useState("");
+  const [questions, setQuestions] = useState("");
+  const [scores, setScores] = useState(0);
 
-  const fetchQuestions=async(categories = "", difficulty = "")=>{
+  const fetchQuestions = async (categories = "", difficulty = "") => {
     const { data } = await axios.get(
       `https://opentdb.com/api.php?amount=10${
         categories && `&category=${categories}`
       }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
     );
-    setQuestions(data.results)
-  }
+    setQuestions(data.results);
+  };
 
   return (
     <div className="App">
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/category" element={<Category names={names} setNames={setNames} fetchQuestions={fetchQuestions}/>} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/category"
+          element={
+            <Category
+              names={names}
+              setNames={setNames}
+              fetchQuestions={fetchQuestions}
+            />
+          }
+        />
         <Route path="/rulespage" element={<RulesPage />} />
-        <Route path="/quiz" element={<Quiz names={names} setNames={setNames} scores={scores} setScores={setScores} questions={questions}
-          setQuestions={setQuestions}/>} />
+        <Route
+          path="/quiz"
+          element={
+            <Quiz
+              names={names}
+              setNames={setNames}
+              scores={scores}
+              setScores={setScores}
+              questions={questions}
+              setQuestions={setQuestions}
+            />
+          }
+        />
       </Routes>
     </div>
   );
