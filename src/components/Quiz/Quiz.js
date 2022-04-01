9af8c8d6ea0f3a6cdb5a97dd53c24421cf19ab8f
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-01 17:24:28 +0530

    Update Quiz.js

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,36 +3,38 @@ import "./Quiz.css";
 import { CircularProgress } from "@material-ui/core";
 import { Questions } from "../Questions/Questions";
 
-export const Quiz = ({ names, scores, setScores, questions,setQuestions}) => {
-    const [options, setOptions] = useState();
-    const [currQues, setCurrQues] = useState(0);
-  
-    const handleShuffle = (optionss) => {
-        return optionss.sort(() => Math.random() - 0.5);
-      };
-  
-   useEffect(() => {
-      setOptions(
-        questions &&
-          handleShuffle([
-            questions[currQues].correct_answer,
-            ...questions[currQues].incorrect_answers
-          ])
-      );
-    }, [currQues, questions]);
-  
+export const Quiz = ({ names, scores, setScores, questions, setQuestions }) => {
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  const handleShuffle = (optionss) => {
+    return optionss.sort(() => Math.random() - 0.5);
+  };
+
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues].correct_answer,
+          ...questions[currQues].incorrect_answers,
+        ])
+    );
+  }, [currQues, questions]);
 
   return (
     <div className="quiz__page">
       <div className="quiz__name">Welcome, {names}</div>
       {questions ? (
         <>
-        <div className="question__flex">
-          {/* <h5 className="question__bold">{questions[currQues].category}</h5> */}
-          <div className="question__num">Question {currQues + 1}<span>/{questions.length}:</span></div>
-         <h5 className="question__bold"> SCORE : {scores}</h5>
-        </div>
-          <Questions 
+          <div className="question__flex">
+            {/* <h5 className="question__bold">{questions[currQues].category}</h5> */}
+            <div className="question__num">
+              Question {currQues + 1}
+              <span>/{questions.length}:</span>
+            </div>
+            <h5 className="question__bold"> SCORE : {scores}</h5>
+          </div>
+          <Questions
             currQues={currQues}
             setCurrQues={setCurrQues}
             questions={questions}
