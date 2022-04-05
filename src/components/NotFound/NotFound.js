c66e8c0f7d7c56a12fe99d55f958eab4b3415f47
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-05 13:30:58 +0530

    added 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NotFound.css";
+import error from "../../assets/images/error.jpg"
 
 export const NotFound = () => {
   return (
     <div className="not__found">
+        <img src={error} alt='error'/>
       Oops!! You have entered a wrong URL.😕{" "}
       <Link to="/" className="span__not">
         {" "}
